d66f9120d57cb71c01843df360ca804c12a2e966
Taqwa <taqwaoumed@gmail.com>
2020-11-04 10:07:00 -0800

    added firebase components

---
+++ scripts/firebase_api_tp.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBDmGKhuBxYlqcqEoB3_z0x3UPqUBTNqEI",
+    authDomain: "socialkids-e7d87.firebaseapp.com",
+    databaseURL: "https://socialkids-e7d87.firebaseio.com",
+    projectId: "socialkids-e7d87",
+    storageBucket: "socialkids-e7d87.appspot.com",
+    messagingSenderId: "43215994463",
+    appId: "1:43215994463:web:25c154c805e49a491065ea"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
