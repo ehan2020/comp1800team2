cd195376ab0607068d03e2520a4bd5bebd99f8b3
PariyaKasaeian <PariyaKasaeian@users.noreply.github.com>
2020-11-24 09:52:38 -0800

    profile and friends css added

--- scripts/friendProfile.js
+++ scripts/friendProfile.js
@@ -4,22 +4,21 @@ function readFriendName(names){
     })
 }
 function readFriendId(ids){
-    db.collection("friends").doc(names).onSnapshot(function(q){
+    db.collection("friends").doc(ids).onSnapshot(function(q){
         document.getElementById("id").innerText = q.data().id;
     })
 }
 
 function readFriendParent(parents){
-    db.collection("friends").doc(names).onSnapshot(function(q){
+    db.collection("friends").doc(parents).onSnapshot(function(q){
         document.getElementById("parent").innerText = q.data().parent;
     })
 }
 
 function readFriendGrade(grades){
-    db.collection("friends").doc(names).onSnapshot(function(q){
+    db.collection("friends").doc(grades).onSnapshot(function(q){
         document.getElementById("grade").innerText = q.data().grade;
     })
 }
 
 
-<script src="scripts/profile.js"> </script>
--- scripts/profile.js
+++ scripts/profile.js
@@ -2,21 +2,22 @@ function readFriendNames(names){
     if(names == "a1"){
         name = readFriendName("a1");
     }
-    if(names == "a2"){
+    else if(names == "a2"){
         name = readFriendName("a2");
     }
-    if(names == "a3"){
+    else if(names == "a3"){
         name = readFriendName("a3");
     }
-    if(names == "d1"){
+    else if(names == "d1"){
         name = readFriendName("d1");
     }
-    if(names == "d2"){
+    else if(names == "d2"){
         name = readFriendName("d2");
     }
-    if(names == "j1"){
+    else if(names == "j1"){
         name = readFriendName("j1");  
     }
+    console.log(name);
     return name;
 }
 
@@ -24,19 +25,19 @@ function readFriendIds(ids){
     if(ids == "a1"){
         id = readFriendId("a1");
     }
-    if(ids == "a2"){
+    else if(ids == a2){
         id = readFriendId("a2");
     }
-    if(ids == "a3"){
+    else if(ids == "a3"){
         id = readFriendId("a3");
     }
-    if(ids == "d1"){
+    else if(ids == "d1"){
         id = readFriendId("d1");
     }
-    if(ids == "d2"){
+    else if(ids == "d2"){
         id = readFriendId("d2");
     }
-    if(ids == "j1"){
+    else if(ids == "j1"){
         id = readFriendId("j1");  
     }
     return id;
@@ -46,19 +47,19 @@ function readFriendParents(parents){
     if(parents == "a1"){
         parent = readFriendParent("a1");
     }
-    if(parents == "a2"){
+    else if(parents == "a2"){
         parent = readFriendParent("a2");
     }
-    if(parents == "a3"){
+    else if(parents == "a3"){
         parent = readFriendParent("a3");
     }
-    if(parents == "d1"){
+    else if(parents == "d1"){
         parent = readFriendParent("d1");
     }
-    if(parents == "d2"){
+    else if(parents == "d2"){
         parent = readFriendParent("d2");
     }
-    if(parents == "j1"){
+    else if(parents == "j1"){
         parent = readFriendParent("j1");  
     }
     return parent;
@@ -68,25 +69,26 @@ function readFriendGrades(grades){
     if(grades == "a1"){
         grade = readFriendId("a1");
     }
-    if(grades == "a2"){
+    else if(grades == "a2"){
         grade = readFriendId("a2");
     }
-    if(grades == "a3"){
+    else if(grades == "a3"){
         grade = readFriendId("a3");
     }
-    if(grades == "d1"){
+    else if(grades == "d1"){
         grade = readFriendId("d1");
     }
-    if(grades == "d2"){
+    else if(grades == "d2"){
         grade = readFriendId("d2");
     }
-    if(grades == "j1"){
+    else if(grades == "j1"){
         grade = readFriendId("j1");  
     }
     return grade;
 }
 
 function readFriend(infos){
+    console.log(infos);
     namess = readFriendNames(infos);
     idss = readFriendIds(infos);
     parentss = readFriendParent(infos);
@@ -97,4 +99,10 @@ function readFriend(infos){
     readFriendGrade(gradess);
 }
 
-<script src="scripts/friendProfile.js"></script>
+function getFriend(){
+    var queryString = decodeURIComponent(window.location.search);
+    var queries = queryString.split("?");   //delimiter
+    var item1 = queries[1];                 //get what's after '?'
+    console.log(item1);
+    readFriend(item1);
+}
