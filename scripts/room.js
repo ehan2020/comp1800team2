f97dacce56f577819ef3bc82576c1e48d714ff50
Taqwa <taqwaoumed@gmail.com>
2020-11-25 15:41:17 -0800

    minor

--- scripts/room.js
+++ scripts/room.js
@@ -1,52 +1,56 @@
-function addActivity(){
+// function addActivity(){
 
-    var activityRef = db.collection("activities");
+//     var activityRef = db.collection("activities");
 
-    activityRef.doc("a-r1").set({
-        room: "1",
-        name: "Coding",
-        category: "Academic"
+//     activityRef.doc("a-r1").set({
+//         room: "1",
+//         name: "Coding",
+//         category: "Academic"
 
-    })
+//     })
 
-    activityRef.doc("a-r2").set({
-        room: "2",
-        name: "Math",
-        category: "Academic" 
-    })
+//     activityRef.doc("a-r2").set({
+//         room: "2",
+//         name: "Math",
+//         category: "Academic" 
+//     })
     
-    activityRef.doc("a-r3").set({
-        room: "3",
-        name: "Science",
-        category: "Academic" 
-    })
+//     activityRef.doc("a-r3").set({
+//         room: "3",
+//         name: "Science",
+//         category: "Academic" 
+//     })
     
-    activityRef.doc("a-r4").set({
-        room: "4",
-        name: "Reading",
-        category: "Academic" 
-    })   
-}
-addActivity();
+//     activityRef.doc("a-r4").set({
+//         room: "4",
+//         name: "Reading",
+//         category: "Academic" 
+//     })   
+// }
+// addActivity();
 
 function readRoomNumber(rooms){
-    if(rooms == "a-r1"){
-        room == readRoomNumber("a-r1");
-    } else if(rooms == "a-r2"){
-        room == readRoomNumber("a-r2");
-    }
+    db.collection("activities").doc(rooms).onSnapshot(function(q){
+        document.getElementById("ar-1").innerText = q.data().name;
+    })
 };
 
+// function readActivity1(number){
+//     db.collection("activities").doc(number).onSnapshot(function(q){
+//         document.getElementById("a1").innerText = q.data().name;
+//     })
+// }
 
 
-getActivity(){
-    db.collection("activities").get().then(function(snap){
-        snap.forEach(function(doc){
-            var n = doc.data().name;
-            console.log(a);
-            document.getElementsByID(abc).innerText=n;
-        })
-    })
 
-}
-getActivity();
+// getActivity(){
+//     db.collection("activities").get().then(function(snap){
+//         snap.forEach(function(doc){
+//             var n = doc.data().name;
+//             console.log(a);
+//             document.getElementsByID(abc).innerText=n;
+//         })
+//     })
+
+// }
+// getActivity();
