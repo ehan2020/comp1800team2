98be934c7f5f2f2253c7401809c24337b5e22553
Taqwa <taqwaoumed@gmail.com>
2020-11-25 15:18:50 -0800

    social and academic rooms

--- scripts/room.js
+++ scripts/room.js
@@ -2,34 +2,49 @@ function addActivity(){
 
     var activityRef = db.collection("activities");
 
-    activityRef.doc("a-1").set({
-        name: "Math",
+    activityRef.doc("a-r1").set({
+        room: "1",
+        name: "Coding",
         category: "Academic"
+
     })
 
-    activityRef.doc("a-2").set({
-        name: "Reading",
+    activityRef.doc("a-r2").set({
+        room: "2",
+        name: "Math",
         category: "Academic" 
     })
     
-    activityRef.doc("a-3").set({
+    activityRef.doc("a-r3").set({
+        room: "3",
         name: "Science",
         category: "Academic" 
     })
     
-    activityRef.doc("a-4").set({
-        name: "Games",
-        category: "Social" 
+    activityRef.doc("a-r4").set({
+        room: "4",
+        name: "Reading",
+        category: "Academic" 
     })   
 }
 addActivity();
+
+function readRoomNumber(rooms){
+    if(rooms == "a-r1"){
+        room == readRoomNumber("a-r1");
+    } else if(rooms == "a-r2"){
+        room == readRoomNumber("a-r2");
+    }
+};
+
+
 
 getActivity(){
     db.collection("activities").get().then(function(snap){
         snap.forEach(function(doc){
             var n = doc.data().name;
             console.log(a);
-            document.getElementsByClassName(card-text).innerText=n;
+            document.getElementsByID(abc).innerText=n;
         })
     })
 
